c2d57b89996f7cccb946c769781af28fdaadf178
fit-s-u-m <nehemiahmaths@gmail.com>
2024-05-14 01:31:33 +0300

    made the front page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,54 @@
-import Image from "next/image";
+"use client";
+import { useEffect } from 'react';
+import Image from 'next/image'
+// import { redirect } from 'next/navigation'
+
 
 export default function Home() {
+  useEffect(() => {
+    const onResize = item => {
+      let item_width = item[0].contentRect.width;
+      if (item_width > window.innerWidth - 100) {
+        window.location.href = "http://localhost:3000/level"
+        // redirect('/level')
+      }
+    }
+    if (typeof document !== 'undefined') {
+      const diffResizer = document.getElementById('diff-resizer');
+      if (diffResizer) {
+        const observer = new ResizeObserver(onResize); // add observer to detect if it resizes
+        observer.observe(diffResizer);
+      } else {
+        console.log("error can not find elment")
+      }
+    }
+
+  }, []);
   return (
-    <button class="btn">
-      Count
-    </button>
-  );
+    <>
+      <div className="diff aspect-[16/9]">
+        <div className="diff-item-1">
+          <Image
+            src="/with_out_nuclear.jpeg"
+            width={500}
+            height={500}
+            alt="With nucler plant"
+          />
+        </div>
+        <div className="diff-item-2">
+          <Image
+            src="/with_nuclear.jpeg"
+            width={500}
+            height={500}
+            alt="with out nuclear plant"
+          />
+        </div>
+        <div className="diff-resizer" id="diff-resizer"></div>
+      </div>
+
+      <button className="btn btn-accent"> Start
+      </button>
+
+    </>
+  )
 }
