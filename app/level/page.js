432ec33b669c374e792a3cf5527679821ff74d12
Fuucodi0X <akmelf20@gmail.com>
2024-05-14 08:14:58 +0300

    fixed typo

--- app/level/page.js
+++ app/level/page.js
@@ -24,7 +24,7 @@ export default function Home() {
       <div className="flex justify-center">
         <div className="card w-96 bg-base-100 shadow-xl">
           <div className="px-10 pt-10 flex justify-center flex-col gap-8 h-full  items-center">
-            <button className="btn btn-outline btn-accent" onClick={go_to_biginner}>Biginner</button>
+            <button className="btn btn-outline btn-accent" onClick={go_to_biginner}>Beginner</button>
             <button className="btn btn-outline btn-accent" onClick={go_to_enthusiast}>Enthusiast</button>
             <button className="btn btn-outline btn-accent" onClick={go_to_itermidiate}>Intermidiate</button>
             <button className="btn btn-outline btn-accent" onClick={go_to_expert}>Expert</button>
@@ -32,7 +32,7 @@ export default function Home() {
           </div>
           <div className="card-body items-center text-center">
             <h2 className="card-title">Levels</h2>
-            <p>if you couldn't know what level you are?</p>
+            <p>if you couldn&#38t know what level you are?</p>
             <div className="card-actions">
               <button className="btn btn-primary" onClick={go_to_quiz}>Take a quick quiz</button>
             </div>
